2e534a7227324053b55a9b7fd80c63b922a394ad
Eva <e.moritz.comcave@gmail.com>
2023-05-12 12:33:34 +0200

    vorl fertig

--- script_01.js
+++ script_01.js
@@ -6,14 +6,31 @@
 0. a+b | a-b | a*b | a/b  --> Ergebnis c 
 
 1. Dateneingabe + -überprüfung : 
-2. Auswahl Rechenart : 
+2. Auswahl Rechenart : check!
 3. Fkt. Grundrechenarten : check!
 4. Ausgabe in Konsole : check!
 */
 
+
+
+
 // Konstanten
 const ERROR_STR_DIV = "Division durch 0 nicht möglich!";
 const ERROR_STR_GEN = "Irgendetwas ging schief!"
+
+// startApp()
+function startApp() {
+	output(calculator(getNum(),getNum(),getOp()));
+}
+
+function getNum() {
+	return parseFloat(prompt("Zahl"));
+}
+
+
+function getOp() {
+	return prompt("Operator");
+}
 
 
 // module: calculator | tests:
@@ -32,20 +49,17 @@ function calculator(a,b,op) {
 		    return div(a,b);
 		default:
 			return ERROR_STR_GEN;
-	}
-
-	
+	}	
 }
 
-agreement : "+","-","*",":","/"
-output(calculator(3,2,"+"));
-output(calculator(3,2,"-"));
-output(calculator(3,2,"*"));
-output(calculator(3,2,":"));
-output(calculator(3,2,"/"));
-output(calculator(3,0,"/"));
-
-output(calculator(3,2,"#?!"));
+// agreement : "+","-","*",":","/"
+// output(calculator(3,2,"+"));
+// output(calculator(3,2,"-"));
+// output(calculator(3,2,"*"));
+// output(calculator(3,2,":"));
+// output(calculator(3,2,"/"));
+// output(calculator(3,0,"/"));
+// output(calculator(3,2,"#?!"));
 
 
 
@@ -73,12 +87,8 @@ function div(a,b) {
 }
 
 
-
 // module: output | test:
 // output()
-
-output(div(3,0));
-output(div(2,-2));
 
 function output(outputData) {
 	console.log(outputData);

ae6f42d1f9ed00a381395fcc8b0307a661d5e158
Eva <e.moritz.comcave@gmail.com>
2023-05-12 09:17:43 +0200

    fkt add test

--- script_01.js
+++ script_01.js
@@ -8,16 +8,28 @@
 1. Dateneingabe + -überprüfung : 
 2. Auswahl Rechenart : 
 3. Fkt. Grundrechenarten : 
-4. Ausgabe in Konsole : 
+4. Ausgabe in Konsole : check!
 */
+
+
+// module: addition a + b | test:
+
+
+function add(a,b) {
+	let c = a + b;
+	return c;
+}
 
 
 
 
 
 // module: output | test:
-// output("hello");
-// output(2);
+// output()
+
+output(add(2,2));
+output(add(2,-2));
+
 function output(outputData) {
 	console.log(outputData);
 }
